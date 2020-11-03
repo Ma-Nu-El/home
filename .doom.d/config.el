727a9dbf97c6289cca46a6163c90694e3b2d70b8
ma <m.fuica01@ufromail.cl>
2020-11-03 13:16:31 -0300

    Add spanish to ispell in .doom.d/config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -107,3 +107,15 @@
 (require 'htmlize)
 
 (setq org-id-link-to-org-use-id t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((ispell-dictionary . "español")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
