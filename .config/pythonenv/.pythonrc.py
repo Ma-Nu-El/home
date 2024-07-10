8ee1d8a77c3c43eaf3c958da8952317911ebae18
Manu <m.fuica01@ufromail.cl>
2024-07-10 18:23:40 -0400

    feat(python): update pythonrc file

--- .config/pythonenv/.pythonrc.py
+++ .config/pythonenv/.pythonrc.py
@@ -1,9 +1,4 @@
-import locale
-
-# Set the locale to your default system locale
-locale.setlocale(locale.LC_ALL, '')
-
-# Define the function to format currency values
-def fmt(value):
-    return locale.currency(value, grouping=True)
-
+def fmt(value, decimal_places=2):
+    format_string = "{:,.%df}" % decimal_places
+    formatted_value = format_string.format(value)
+    return formatted_value
