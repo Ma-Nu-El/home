8b745ff15f0e4af66979c503260e7bf30fcec59d
Manu <m.fuica01@ufromail.cl>
2024-07-30 17:31:46 -0400

    feat(pandoc): update config

--- .config/pandoc/pre-config.sh
+++ .config/pandoc/pre-config.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# - [2024-07-30 Tue 17:30]
+# This script creates dynamically a config file for
+# pandoc. This is because pandoc can not read system
+# variables from a config file.
 
 # Default to 80 if the environment variable is not set
 LINE_LENGTH=${CUSTOM_CLI_LINE_LENGTH:-80}
