eef1389aa6e8ad59d754c3ce99037de83d2fa6e0
Manu <m.fuica01@ufromail.cl>
2025-02-16 18:36:15 -0300

    feat(doom): enable modeline module

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -12,7 +12,8 @@ doom-dashboard    ; a nifty splash screen for Emacs
 doom-quit         ; DOOM quit-message prompts when you quit Emacs
 
 hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-(modeline +light)          ; snazzy, Atom-inspired modeline, plus API
+;;(modeline +light)          ; snazzy, Atom-inspired modeline, plus API
+modeline          ; snazzy, Atom-inspired modeline, plus API
 
 nav-flash         ; blink cursor line after big motions
 ophints           ; highlight the region an operation acts on
